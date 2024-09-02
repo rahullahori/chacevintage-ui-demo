6d8dd6276f5fdc0ddca3f9747d64a3731ae08c9d
rahullahori <rgprahul@gmail.com>
2024-09-02 18:37:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Welcome to ChaceVinatge fashion.. this site is under construction, will be up and running soon....
+      </div>
+      
+    </>
+  )
+}
+
+export default App
